e703f62c39f38bb9ff32d30a1d5b89feb2ecbb11
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-05-08 05:05:14 +0000

    Fix typo and add main

--- app.py
+++ app.py
@@ -14,9 +14,12 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/open-door', method=['POST'])
+@app.route('/open-door', methods=['POST'])
 def open_door():
     GPIO.output(11, True)
     time.sleep(5)
     GPIO.output(11, False)
     return "The door should be open"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80)
