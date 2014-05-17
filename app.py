42813ecceb3a13431b87a5abba8a1d2bbbc61542
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-05-17 04:46:40 +0000

    A multithreaded version update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 from flask import Flask, render_template, redirect
 import RPi.GPIO as GPIO
@@ -9,17 +10,26 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 GPIO.output(11, True)
 
+thread_lock = threading.Lock()
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
+def open_and_lock_after(timeout=5):
+    """
+    :param timeout: The time after which the door should be locked
+    """
+    with thread_lock:
+        GPIO.output(11, False)
+        time.sleep(5)
+        GPIO.output(11, True)
+
 
 @app.route('/open-door', methods=['POST'])
 def open_door():
-    GPIO.output(11, False)
-    time.sleep(5)
-    GPIO.output(11, True)
+    thread = threading.Thread(target=open_and_lock_after)
+    thread.start()
     return redirect('/?opened=true')
 
 
